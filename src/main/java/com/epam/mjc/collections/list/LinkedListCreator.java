7794bb5bbf30fddaeb887c5eef7be950ae2ce7b6
romikokvinikadze <r.kvinikadze11@gmail.com>
2022-08-19 07:33:19 +0400

    lists

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> res = new LinkedList<>();
+        for (int elem: sourceList){
+            if (elem % 2 != 0){
+                res.addFirst(elem);
+            }else
+                res.addLast(elem);
+        }
+
+        return res;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,24 @@
 package com.epam.mjc.collections.list;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        Collections.sort(sourceList, new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int aInt = Integer.parseInt(a);
+        int bInt = Integer.parseInt(b);
+        int fa = (int) Math.pow(5 * Integer.parseInt(a), 2) + 3;
+        int fb = (int) Math.pow(5 * Integer.parseInt(b), 2) + 3;
+
+        return fa == fb ? aInt - bInt : fa - fb;
     }
 }
